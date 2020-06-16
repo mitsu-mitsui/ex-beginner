f223935c3d130c5cda121d4257984bc61dd8c6f7
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-16 15:29:27 +0900

    form画面の表示の実装、画面遷移

--- src/main/java/com/example/Controller/Exam02Controller.java
+++ src/main/java/com/example/Controller/Exam02Controller.java
@@ -1,6 +1,7 @@
 package com.example.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +11,17 @@ public class Exam02Controller {
 	@RequestMapping("")
 	public String index() {
 		return "exam02" ;
+	}
+	
+	@RequestMapping("/input")
+	public String input(Integer n1,Integer n2 ,Model model) {
+		
+		int sum = n1+n2;
+		model.addAttribute("n1", n1);
+		model.addAttribute("n2",n2);
+		model.addAttribute("sum",sum);
+		
+		return "exam02-result";
 	}
 	
 }
