c74df857c7ea1a426b70959565025f55c73a4b73
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-16 14:48:14 +0900

    初期画面へ遷移できるようにした（Controller）

--- src/main/java/com/example/Controller/Exam01Controller.java
+++ src/main/java/com/example/Controller/Exam01Controller.java
@@ -1,5 +1,15 @@
 package com.example.Controller;
 
-public class Exam01Controller {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller 
+@RequestMapping("ex1")
+public class Exam01Controller {
+	
+	@RequestMapping("")
+	public String index() {
+		
+		return "exam01";
+	}
 }
