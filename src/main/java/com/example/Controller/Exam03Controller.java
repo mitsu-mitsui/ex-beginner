16c237c3d732a3b5005896025d1c28ff5137d1d6
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-16 16:02:30 +0900

    ex3 input処理の実装

--- src/main/java/com/example/Controller/Exam03Controller.java
+++ src/main/java/com/example/Controller/Exam03Controller.java
@@ -1,5 +1,8 @@
 package com.example.Controller;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,10 +10,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/ex3")
 public class Exam03Controller {
 	
+	@Autowired
+	private ServletContext application;
+	
 	@RequestMapping("")
 	public String index() {
 		return "exam03";
 	}
 	
+	@RequestMapping("/input")
+	public String input(Integer item1,Integer item2,Integer item3) {
+		
+		int sum = item1+item2+item3;
+		int sumplusTax =(int)(sum*1.1); 
+		
+		application.setAttribute("item1", item1);
+		application.setAttribute("item2", item2);
+		application.setAttribute("item3", item3);
+		application.setAttribute("sum", sum);
+		application.setAttribute("sumplusTax", sumplusTax);
+		
+		return "exam03-result";
+	}
 	
 }
