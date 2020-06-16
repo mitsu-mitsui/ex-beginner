222c99a0602191d0a5b8daa5952edbcb8156f504
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-16 17:00:31 +0900

    ex4 input処理の実装完了

--- src/main/java/com/example/Controller/Exam04Controller.java
+++ src/main/java/com/example/Controller/Exam04Controller.java
@@ -1,11 +1,21 @@
 package com.example.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.User;
+import com.example.form.UserForm;
 
 @Controller
 @RequestMapping("/ex4")
 public class Exam04Controller {
+	
+	@ModelAttribute
+	public UserForm setUpForm() {
+		return new UserForm() ;
+	}
 	
 	@RequestMapping("")
 	public String index() {
@@ -13,8 +23,15 @@ public class Exam04Controller {
 	}
 	
 	@RequestMapping("/input")
-	public String input()
-	{
+	public String input(UserForm form ,Model model){
+		User user = new User();
+		
+		user.setAge(form.getAge());
+		user.setName(form.getName());
+		user.setComment(form.getComment());
+
+		model.addAttribute("user", user);
+		
 		return "exam04-result";
 	}
 }
