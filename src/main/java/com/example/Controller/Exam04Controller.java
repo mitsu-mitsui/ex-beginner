ac21d57366e41edb60c0bca1f8e29896ecde6715
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-16 16:41:17 +0900

    ex4　空html,domain,form,controller作成

---
+++ src/main/java/com/example/Controller/Exam04Controller.java
@@ -0,0 +1,10 @@
+package com.example.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ex4")
+public class Exam04Controller {
+
+}
