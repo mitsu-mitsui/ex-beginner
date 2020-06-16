c0faffa86d2ce6a9a103184e29c7180fbae464b0
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-16 15:31:45 +0900

    ふぃっくす：model ->session

--- src/main/java/com/example/Controller/Exam02Controller.java
+++ src/main/java/com/example/Controller/Exam02Controller.java
@@ -1,5 +1,8 @@
 package com.example.Controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,18 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("ex2")
 public class Exam02Controller {
 
+	@Autowired
+	private HttpSession session;
+	
 	@RequestMapping("")
 	public String index() {
 		return "exam02" ;
 	}
 	
 	@RequestMapping("/input")
-	public String input(Integer n1,Integer n2 ,Model model) {
+	public String input(Integer n1,Integer n2 ) {
 		
 		int sum = n1+n2;
-		model.addAttribute("n1", n1);
-		model.addAttribute("n2",n2);
-		model.addAttribute("sum",sum);
+		session.setAttribute("n1", n1);
+		session.setAttribute("n2",n2);
+		session.setAttribute("sum",sum);
 		
 		return "exam02-result";
 	}
